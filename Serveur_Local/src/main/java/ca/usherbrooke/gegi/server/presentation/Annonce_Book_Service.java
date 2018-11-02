fa753556742d3f59c5b8f128f6087a9c68ca6f7e
simkass <43049319+simkass@users.noreply.github.com>
2018-11-02 11:35:40 -0400

    Shrek_ADD

--- Serveur_Local/src/main/java/ca/usherbrooke/gegi/server/presentation/Annonce_Book_Service.java
+++ Serveur_Local/src/main/java/ca/usherbrooke/gegi/server/presentation/Annonce_Book_Service.java
@@ -7,12 +7,13 @@ import ca.usherbrooke.gegi.server.persistence.Annonce_Mapper;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import java.util.List;
+
 
 @Path("annonce")
 public class Annonce_Book_Service {
@@ -24,10 +25,11 @@ public class Annonce_Book_Service {
 
     Annonce_Book_Mapper annonce_Book_Mapper;
 
-
-
     @Path("/add_annonce")
-    public void anonce_Add(){
+    public void anonce_Add(@QueryParam("cip") String cip, @QueryParam("type_ad_id") Integer type_ad_id, @QueryParam("titre_annonce") String titre_annonce,
+                           @QueryParam("description") String description, @QueryParam("prix") double prix, @QueryParam("contact") String contact,
+                           @QueryParam("auteur") String auteur, @QueryParam("ISBN") String ISBN, @QueryParam("edition") String edition,
+                           @QueryParam("titre") String titre, @QueryParam("course_id") String course_id){
         System.out.println("annonce add ok test 1 int +++++++++++++++++");
 
 
@@ -35,25 +37,20 @@ public class Annonce_Book_Service {
 
         //annonceBook.setAd_id(10);
         //annonceBook.set_categorie("itm01");
-        annonceBook.setCip("ihab0001");
-        annonceBook.setType_ad_id(1);
-        annonceBook.set_titre_annonce("Annonce_test pour livre de prog mybatis +sql");
-        annonceBook.set_description("test livre de prog mybatis +sql ");
-        annonceBook.set_prix(99.9);
-        annonceBook.setContact("ciptest02222@gmail.com");
-        annonceBook.setAuteur("auteur_test");
-        annonceBook.setISBN("ISBN0000000001");
-        annonceBook.setEdition("Edition 2018");
-        annonceBook.setTitre("programmation java et SQL");
-        annonceBook.set_cours_id("GIF332");
-
-
+        annonceBook.setCip(cip);
+        annonceBook.setType_ad_id(type_ad_id);
+        annonceBook.set_titre_annonce(titre_annonce);
+        annonceBook.set_description(description);
+        annonceBook.set_prix(prix);
+        annonceBook.setContact(contact);
+        annonceBook.setAuteur(auteur);
+        annonceBook.setISBN(ISBN);
+        annonceBook.setEdition(edition);
+        annonceBook.setTitre(titre);
+        annonceBook.set_cours_id(course_id);
 
         annonce_Book_Mapper.insertAnnonce_Book(annonceBook);
         System.out.println("annonce add ok test 2 out ++++++++++++++++++");
-
-
-
 
 
     }
